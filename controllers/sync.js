1785a1ad0c6516faa76ace57d2cde580974f07d9
talat mahmood <talatm02@gmail.com>
2019-06-09 16:56:37 +0400

    sync.js; code commented for integrator

--- controllers/sync.js
+++ controllers/sync.js
@@ -1,18 +1,18 @@
-var Integrator = require('okasintegrator');
-var secrets = require('../config/secrets');
-var ObjectId = require('mongoose').Schema.ObjectId; 
+// var Integrator = require('okasintegrator');
+// var secrets = require('../config/secrets');
+// var ObjectId = require('mongoose').Schema.ObjectId; 
 
-var integrator = new Integrator({
-    connectionString:secrets.db,
-    parserPath: __dirname+"/../integrators/",
-});
+// var integrator = new Integrator({
+//     connectionString:secrets.db,
+//     parserPath: __dirname+"/../integrators/",
+// });
 
 exports.startSync = function(req,res,next){
-	var entity = req.params.entity;
-	var provider_id = req.user.provider_id;
-	integrator.syncNow(provider_id,entity,function(err){
-		if(err)
-			return next(err);
-		return res.status(200).json({'msg':'Sync started'});
-	});
+	// var entity = req.params.entity;
+	// var provider_id = req.user.provider_id;
+	// integrator.syncNow(provider_id,entity,function(err){
+	// 	if(err)
+	// 		return next(err);
+	// 	return res.status(200).json({'msg':'Sync started'});
+	// });
 }
